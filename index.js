ed837296265d7d2e94e3e66fdd53d77aac75f71c
Mark Simoncic <63131950+masimoncic@users.noreply.github.com>
2020-12-26 20:09:05 -0500

    bugs fixed

--- index.js
+++ index.js
@@ -1,97 +1,17 @@
+const lexer = new PropositionalLogicLexer();
 
-class Lexer {
-  constructor () {
-
-    this.pos = 0;
-    this.buf = null;
-    this.buflen = 0;
-
-    this.logicalSymbolsSingle = {
-      '~' : 'NOT',
-      '&' : 'AND',
-      '|' : 'OR',
-      '(' : 'L_PAREN',
-      ')' : 'R_PAREN',
-    }
-
-    this.logicalSymbolsDouble = {
-      '->' : 'CONDITIONAL',
-      '==' : 'BICONDITIONAL',
-    }
-
+function makeList() {
+  let input = document.getElementById('input');
+  const list = document.getElementById('list');
+  let tokens = lexer.tokenAll(input.value);
+  list.innerHTML = '';
+  for(let i = 0; i < tokens.length; i++) {
+    let li = document.createElement('li');
+    let temp = tokens[i];
+    li.innerText = `name: ${temp.name}, value: ${temp.value}, pos: ${temp.pos}`
+    list.appendChild(li);
   }
-
-  input (buf) {
-    this.pos = 0;
-    this.buf = buf;
-    this.buflen = buf.length;
-  }
-
-  token() { 
-    //first check that there are still characters left
-    if (this.pos > buflen) {
-      return null;
-    }
-
-    let c = this.buf[this.pos];
-    let sym = this.logicalSymbolsSingle[c];
-    let cPlus = this.buf.slice(this.pos, this.pos+2);
-    let symPlus = this.logicalSymbolsDouble[c];
-    if (sym !== undefined) {
-      this.pos ++;
-      return {
-        name: sym,
-        value: c,
-        pos: this.pos
-        }
-      }
-    else if (symPlus !== defined) {
-      this.pos += 2;
-      return {
-        name: sym,
-        value: cPlus,
-        pos: this.pos
-        }
-      }
-    //not a logical symbol
-    else {
-      //getEndPos defined below
-      let endPos = this.getEndPos(startPos);
-      return {
-        name:'PROPOSITION',
-        value: this.buff.slice[startPos, endPos+1],
-        pos: endPos +1
-      }
-    };
-  }
-
-  getEndPos (startPos) {
-    let logicalSyms = ['~', '&', '|', '(', ')', '-', '='];
-    let endPos = startPos +1;
-    while(endPos < this.buflen) {
-      let c = this.buff[endPos];
-      if (logicalSyms.includes(c)) {
-        if ((c === '-' && this.buff[endPos+1] !== '>') || (c === '=' && this.buff[endPos+1] !== '=')) {
-          endPos++;
-        }
-        return endPos;
-      }
-      else {
-        endPos++;
-      }
-
-    }
-    return endPos;
-  }
-
-  tokenAll (str) {
-    tokenArr = [];
-    this.input(str);
-    while (this.pos > this.buflen) {
-      tokenArr.push(this.token());
-    }
-
-  }
-
-
 }
+
+const button = document.getElementById('lexButton');
+button.addEventListener('click', makeList);
--- lexer.js
+++ lexer.js
@@ -1,15 +1,16 @@
 
-class Lexer {
+class PropositionalLogicLexer {
   constructor () {
 
     this.pos = 0;
-    this.buf = null;
+    this.buf = '';
     this.buflen = 0;
+    this.endPos = 0;
 
     this.logicalSymbolsSingle = {
-      '~' : 'NOT',
-      '&' : 'AND',
-      '|' : 'OR',
+      '~' : 'NEGATION',
+      '&' : 'CONJUNCTION',
+      '|' : 'DISJUNCTION',
       '(' : 'L_PAREN',
       ')' : 'R_PAREN',
     }
@@ -21,22 +22,22 @@ class Lexer {
 
   }
 
-  input (buf) {
+  input (str) {
     this.pos = 0;
-    this.buf = buf;
-    this.buflen = buf.length;
+    this.buf = str;
+    this.buflen = str.length;
   }
 
   token() { 
     //first check that there are still characters left
-    if (this.pos > buflen) {
+    if (this.pos > this.buflen) {
       return null;
     }
 
     let c = this.buf[this.pos];
     let sym = this.logicalSymbolsSingle[c];
     let cPlus = this.buf.slice(this.pos, this.pos+2);
-    let symPlus = this.logicalSymbolsDouble[c];
+    let symPlus = this.logicalSymbolsDouble[cPlus];
     if (sym !== undefined) {
       this.pos ++;
       return {
@@ -45,22 +46,26 @@ class Lexer {
         pos: this.pos
         }
       }
-    else if (symPlus !== defined) {
+    else if (symPlus !== undefined) {
+      let tempPos = this.pos +1;
       this.pos += 2;
       return {
-        name: sym,
+        name: symPlus,
         value: cPlus,
-        pos: this.pos
+        pos: tempPos,
         }
       }
     //not a logical symbol
     else {
       //getEndPos defined below
-      let endPos = this.getEndPos(startPos);
+      this.endPos = this.getEndPos(this.pos);
+      let prop = this.buf.slice(this.pos, this.endPos);
+      let tempPos = this.pos +1;
+      this.pos = this.endPos;
       return {
         name:'PROPOSITION',
-        value: this.buff.slice[startPos, endPos+1],
-        pos: endPos +1
+        value: prop,
+        pos: tempPos
       }
     };
   }
@@ -69,12 +74,11 @@ class Lexer {
     let logicalSyms = ['~', '&', '|', '(', ')', '-', '='];
     let endPos = startPos +1;
     while(endPos < this.buflen) {
-      let c = this.buff[endPos];
+      let c = this.buf[endPos];
       if (logicalSyms.includes(c)) {
-        if ((c === '-' && this.buff[endPos+1] !== '>') || (c === '=' && this.buff[endPos+1] !== '=')) {
-          endPos++;
-        }
-        return endPos;
+        if (!((c === '-' && this.buf[endPos+1] !== '>') || (c === '=' && this.buf[endPos+1] !== '='))) {
+          return endPos;}
+        else {endPos++;}
       }
       else {
         endPos++;
@@ -85,12 +89,12 @@ class Lexer {
   }
 
   tokenAll (str) {
-    tokenArr = [];
+    let tokenArr = [];
     this.input(str);
-    while (this.pos > this.buflen) {
+    while (this.pos < this.buflen) {
       tokenArr.push(this.token());
     }
-
+    return tokenArr;
   }
 
 
